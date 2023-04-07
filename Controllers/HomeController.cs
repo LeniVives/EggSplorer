1ba7cabd1df92f85ab0ea2ea762836b95ab2f776
Jarno-Pot <96240823+Jarno-Pot@users.noreply.github.com>
2023-04-07 16:01:29 +0200

    Sorteren op prijs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -45,13 +45,19 @@ namespace EggSplorer.Controllers
         {
             var _context = new EggsplorerContext();
             ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            //ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["PriceSortParm"] = sortOrder == "Price" ? "price_desc" : "Price";
             var products = from s in _context.Products
                            select s;
             switch (sortOrder)
             {
                 case "name_desc":
                     products = products.OrderByDescending(s => s.Name);
+                    break;
+                case "Price":
+                    products = products.OrderBy(s => s.ProductPrice);
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(s => s.ProductPrice);
                     break;
                 default:
                     products = products.OrderBy(s => s.Name);
