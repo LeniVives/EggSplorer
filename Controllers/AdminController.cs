b6ac39f71eb63c8462b17d5b97f58eccc42e3b04
Jarno-Pot <96240823+Jarno-Pot@users.noreply.github.com>
2023-04-12 15:58:56 +0200

    Admin controller toevoegen

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EggSplorer.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult bEdit()
+        {
+            return View();
+        }
+        public IActionResult bDelete()
+        {
+            return View();
+        }
+
+        //-------------------------------------------------------------------
+
+        public IActionResult gCreate()
+        {
+            return View();
+        }
+        public IActionResult gEdit()
+        {
+            return View();
+        }
+        public IActionResult gDelete()
+        {
+            return View();
+        }
+
+        //-------------------------------------------------------------------
+
+        public IActionResult pCreate()
+        {
+            return View();
+        }
+        public IActionResult pEdit()
+        {
+            return View();
+        }
+        public IActionResult pDelete()
+        {
+            return View();
+        }
+    }
+}
